7664cf95066c0b5b13eb94676c154170a3dee338
mimi <afsana.mimi0304@gmail.com>
2020-05-04 18:19:29 +0600

    first commit

---
+++ newuser_destination.php
@@ -0,0 +1,71 @@
+<?php 
+
+
+echo"<br \>";
+echo"<br \>";
+
+$host="localhost";
+$dbuser="root";
+$pass="";
+$dbname="delivery";
+$conn=mysqli_connect($host,$dbuser,$pass,$dbname);
+if(mysqli_connect_errno())
+{
+	die("connextion failed....!!!!" .mysqli_connect_errno());
+}
+?>
+
+<html>
+<head>
+	<title>data adding</title>
+</head>
+<body>
+	<?php
+    
+    if(isset($_POST['submit']))
+    {    
+
+
+    	$first=$_POST['first_name'];
+    	$last=$_POST['last_name'];
+    	$email=$_POST['email'];
+    	$phone=$_POST['phone_number'];
+    	$pass=$_POST['password'];
+    	$cpass=$_POST['confirmpassword'];
+    	
+     
+        if($pass==$cpass)
+        {
+
+        	echo "password confirmed";
+
+        	$sql="insert into user(first_name,last_name,email,phone,password,confirm_password)"."values('$first','$last','$email','$phone','$pass','$cpass')";
+        $res=mysqli_query($conn,$sql);
+
+        if(!$res)
+        {
+        	die("queri failed error.....!!!!!!!!");
+        }
+
+        else{ echo "Registation Successful";
+
+    }
+
+        }
+
+        else {
+      
+        	echo "Password Didn't Match, You will be redirected to login page after 2 sec";
+
+        	header("refresh:2; url=newuser(source).php");
+        }
+       
+
+    }
+
+	?>
+	
+
+
+</body>
+</html>
---
+++ newuser_source.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<title>
+		WELCOME TO LUNCHBOX
+	</title>
+	</head>
+
+	<body>
+		
+			<form action="newuser(destination).php" method="POST">
+              <tr>
+              	<td>First Name:</td>
+              	<td> <input type="text" name="first_name" placeholder="first name"></td>
+              </tr>
+              
+              <tr>
+              	<td>Last Name:</td>
+              	<td><input type="text" name="last_name" placeholder="last name"></td>
+              </tr>
+                 <br>
+              <tr>
+              	<td>Email:</td>
+              	<td><input type="text" name="email" placeholder="e-mail" </td>
+              </tr>
+           <br>
+              	<tr>
+              		<td>Phone Number:</td>
+              		<td><input type="text" name="phone_number"</td>
+              	</tr>
+ <br>
+              	 <tr>
+              	<td>Password:</td>
+              	<td><input type="password" name="password" placeholder="choose a strong password" </td>
+              </tr>
+ <br>
+              <tr>
+              	<td> Confirm Password:</td>
+              	<td><input type="passsword" name="confirmpassword" placeholder="retype your password" </td>
+              </tr>
+
+    
+              <tr><td><input type="submit" name="submit" value="submit"></td></tr>
+
+
+              
+
+			</form>
+			
+
+		</table>
+	</body>
+
+</html>
